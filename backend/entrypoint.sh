d1701a2000802a0c50a5789b9317c24e0ce5a1b0
mstva <mustafa.abdulluh@gmail.com>
2022-12-07 19:52:32 +0200

    added docker and docker compose

---
+++ backend/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+cd /backend/
+. /opt/venv/bin/activate
+
+echo "[entrypoint.sh] Apply migrations"
+flask db init || true
+flask db migrate
+flask db upgrade
+
+echo "[entrypoint.sh] Start the server"
+gunicorn -b 0.0.0.0 'app:create_app()'
