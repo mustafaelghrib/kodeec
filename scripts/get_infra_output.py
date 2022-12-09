c7e66e618ff47f061c91beb1d5e1c0623a527384
mstva <mustafa.abdulluh@gmail.com>
2022-12-09 19:22:12 +0200

    added output format

--- scripts/get_infra_output.py
+++ scripts/get_infra_output.py
@@ -4,13 +4,15 @@ import subprocess
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--compose", help="Docker Compose File")
-parser.add_argument("--module", help="Output Module")
+parser.add_argument("--c", help="Docker Compose File")
+parser.add_argument("--m", help="Output Module")
+parser.add_argument("--f", help="Output Format")
 
 args = parser.parse_args()
 
-compose = args.compose
-module = args.module
+compose = args.c
+module = args.m
+output_format = args.f
 
 command = f'docker compose -f {compose} run --rm terraform output -json {module}'
 process = subprocess.run(command, shell=True, stdout=subprocess.PIPE)
@@ -19,6 +21,9 @@ result = process.stdout.decode('utf-8')
 try:
     output = json.loads(result)
     for item in output:
-        print(f"{item}={output[item]}")
+        if output_format == "env":
+            print(f"{item}={output[item]}")
+        elif output_format == "github":
+            print(f'{item} = "{output[item]}"')
 except Exception as e:
     print(f'No output for {module} | {e}\n')
