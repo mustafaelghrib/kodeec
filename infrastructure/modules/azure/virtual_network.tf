26636a082b2d69a7fc9d347a34cb20a33ea0c529
mstva <mustafa.abdulluh@gmail.com>
2022-12-09 18:52:49 +0200

    added ssh and http security rules

--- infrastructure/modules/azure/virtual_network.tf
+++ infrastructure/modules/azure/virtual_network.tf
@@ -31,19 +31,34 @@ resource "azurerm_network_security_group" "main" {
   name                = var.network_security_group_name
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
+}
 
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
+resource "azurerm_network_security_rule" "ssh" {
+  name                        = "SSH"
+  priority                    = 1001
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.main.name
+}
 
+resource "azurerm_network_security_rule" "http" {
+  name                        = "HTTP"
+  priority                    = 1011
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.main.name
+  network_security_group_name = azurerm_network_security_group.main.name
 }
 
 resource "azurerm_network_interface" "main" {
